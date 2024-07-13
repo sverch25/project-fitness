a7306bc0ac3eea027e5a150df60aca9247a36774
sverch25 <zaichko@list.ru>
2024-07-13 16:54:37 +0500

    add styles

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,12 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+const btnElement = document.querySelector('.hero__btn');
+const offersElement = document.querySelector('.offers');
+
+btnElement.addEventListener('click', () => {
+  offersElement.scrollIntoView({
+    block: 'nearest',
+    behavior: 'smooth',
+  });
+});
